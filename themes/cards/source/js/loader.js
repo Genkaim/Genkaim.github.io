53ade0f5b5d619ca2785bb0fe6c2fd1ba7da155e
genkaim <cxh2066172407@outlook.com>
2024-09-21 14:08:22 +0800

    genkaim_backup

---
+++ themes/cards/source/js/loader.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  // DOM 加载完成时执行的函数
+  setTimeout(function(){
+        const blur_cover = document.querySelector("#loader");
+        blur_cover.style.display = "none";
+    }, 1300);
+});
