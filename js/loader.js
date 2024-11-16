8bdd8af6ca083401714086a14317723a4d5c7814
genkaim <cxh2066172407@outlook.com>
2024-11-16 13:39:44 +0800

    Site updated: 2024-11-16 13:39:43

--- js/loader.js
+++ js/loader.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function () {
     const blur_cover = document.querySelector("#loader");
     blur_cover.style.animation = "blurTransition 1.3s forwards ease";
-    document.querySelector(".hello-parent").style.opacity  ="0";
+    document.querySelector(".loader_show").style.opacity  ="0";
     // DOM 加载完成时执行的函数
     setTimeout(function () {
       const blur_cover = document.querySelector("#loader");
