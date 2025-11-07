474fbf857ba04e3b0f0403b32366210c1440ddf5
Genkaim <cxh2066172407@outlook.com>
2025-11-07 16:06:55 +0800

    Site updated: 2025-11-07 16:06:52

---
+++ js/loader.js
@@ -0,0 +1,14 @@
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+      const blur_cover = document.querySelector("#loader");
+      blur_cover.style.animation = "blurTransition 1.3s forwards ease";
+      document.querySelector(".loader_show").style.opacity  ="0";
+      // DOM 加载完成时执行的函数
+      setTimeout(function () {
+        const blur_cover = document.querySelector("#loader");
+        blur_cover.style.display = "none";
+      }, 1000);
+    }, 1100);
+  });
+*/
